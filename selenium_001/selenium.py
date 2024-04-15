a156b812bceb5b02e2f521056d0745be248d977e
Manisha <manishakataria.oct31@gmail.com>
2024-04-15 01:10:37 -0400

    tst

--- selenium_001/selenium.py
+++ selenium_001/selenium.py
@@ -1,3 +1,6 @@
+import time
+
+import driver
 import pytest
 
 from selenium import webdriver
@@ -7,8 +10,29 @@ from selenium.webdriver.common.by import By
 
 def test_opengoogle():
   driver = webdriver.Chrome()
-  driver.get("http://google.com")
-  driver.maximize_window()
+  driver.get("https://katalon-demo-cura.herokuapp.com/")
+  makeappt_element = driver.find_element(By.ID, "btn-make-appointment")
+  makeappt_element.click()
+  time.sleep(5)
+  assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/profile.php#login"
+  time.sleep(3)
+
+  #username and password
+  username = driver.find_element(By.ID, "txt-username")
+  username.send_keys("John Doe")
+  password = driver.find_element(By.ID, "txt-password")
+  password.send_keys("ThisIsNotAPassword")
+  click_login = driver.find_element(By.ID, "btn-login")
+  click_login.click()
+  time.sleep(5)
+  # Make Appointment
+  page_title = driver.title
+  print(page_title)
+
+  text_verify = driver.find_element(By.CLASS_NAME,"col-sm-12")
+  print(text_verify)
+  assert text_verify.text == "Make Appointment"
+
   #hi
   #driver.find_element(by=By.ID, "q").send_keys("")
 
